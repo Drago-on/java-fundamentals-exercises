87879b20300b8b869ca962b3216b9241188899d6
Drago-on <whoareyouwhenyouare@gmail.com>
2024-05-29 16:43:31 +0300

    Task 2-2-1 completed

--- 2-0-data-structures-and-algorithms/2-2-1-node/src/main/java/com/bobobode/cs/Node.java
+++ 2-0-data-structures-and-algorithms/2-2-1-node/src/main/java/com/bobobode/cs/Node.java
@@ -1,5 +1,7 @@
 package com.bobobode.cs;
 
+import lombok.Setter;
+
 /**
  * Class {@link Node} is a very simple data structure that consists of an element itself and the reference to the next
  * node. An element can have any value since it's a generic. A reference to the next node allows to link {@link Node}
@@ -9,5 +11,20 @@ package com.bobobode.cs;
  * @author Taras Boychuk
  */
 public class Node<T> {
-    // todo:
+    public T element;
+    @Setter
+    private Node<T> next;
+
+    public Node(T element) {
+        this.element = element;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
 }
--- 2-0-data-structures-and-algorithms/2-2-1-node/src/main/java/com/bobobode/cs/Nodes.java
+++ 2-0-data-structures-and-algorithms/2-2-1-node/src/main/java/com/bobobode/cs/Nodes.java
@@ -22,7 +22,7 @@ public class Nodes {
      * @return a new instance of {@link Node}
      */
     public static <T> Node<T> create(T element) {
-        throw new ExerciseNotCompletedException(); // todo:
+        return new Node<>(element);
     }
 
     /**
@@ -33,7 +33,7 @@ public class Nodes {
      * @param <T>    a genetic type
      */
     public static <T> void link(Node<T> first, Node<T> second) {
-        throw new ExerciseNotCompletedException(); // todo:
+        first.setNext(second);
     }
 
     /**
@@ -46,7 +46,10 @@ public class Nodes {
      * @return a reference to a first node created based on firstElement
      */
     public static <T> Node<T> pairOf(T firstElement, T secondElement) {
-        throw new ExerciseNotCompletedException(); // todo:
+        Node<T> first = new Node<>(firstElement);
+        Node<T> second = new Node<>(secondElement);
+        first.setNext(second);
+        return first;
     }
 
     /**
@@ -60,7 +63,11 @@ public class Nodes {
      * @return a reference to the first node
      */
     public static <T> Node<T> closedPairOf(T firstElement, T secondElement) {
-        throw new ExerciseNotCompletedException(); // todo:
+        Node<T> first = new Node<>(firstElement);
+        Node<T> second = new Node<>(secondElement);
+        first.setNext(second);
+        second.setNext(first);
+        return first;
     }
 
     /**
@@ -72,7 +79,17 @@ public class Nodes {
      * @return a reference to the first element of the chain
      */
     public static <T> Node<T> chainOf(T... elements) {
-        throw new ExerciseNotCompletedException(); // todo:
+        if (elements == null || elements.length == 0) {
+            return null;
+        }
+        Node<T> head = new Node<>(elements[0]);
+        Node<T> current = head;
+        for (int i = 1; i < elements.length; i++) {
+            Node<T> next = new Node<>(elements[i]);
+            current.setNext(next);
+            current = next;
+        }
+        return head;
     }
 
     /**
@@ -85,6 +102,17 @@ public class Nodes {
      * @return a reference to the first element of the chain
      */
     public static <T> Node<T> circleOf(T... elements) {
-        throw new ExerciseNotCompletedException(); // todo:
+        if (elements == null || elements.length == 0) {
+            return null;
+        }
+        Node<T> head = new Node<>(elements[0]);
+        Node<T> current = head;
+        for (int i = 1; i < elements.length; i++) {
+            Node<T> next = new Node<>(elements[i]);
+            current.setNext(next);
+            current = next;
+        }
+        current.setNext(head);
+        return head;
     }
 }
