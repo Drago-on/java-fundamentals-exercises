799ca8d537950eebb39224fc593803b9b766d29d
Drago-on <whoareyouwhenyouare@gmail.com>
2024-05-29 17:20:45 +0300

    Task 3-6-1 completed

--- 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
+++ 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
@@ -2,6 +2,13 @@ package com.bobocode.se;
 
 import com.bobocode.util.ExerciseNotCompletedException;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * {@link FileReaders} provides an API that allow to read whole file into a {@link String} by file name.
  */
@@ -14,6 +21,16 @@ public class FileReaders {
      * @return string that holds whole file content
      */
     public static String readWholeFile(String fileName) {
-        throw new ExerciseNotCompletedException(); //todo
+        URL resource = FileReaders.class.getClassLoader().getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("File not found: " + fileName);
+        }
+        try {
+            Path path = Paths.get(resource.toURI());
+            String content = Files.readString(path);
+            return content.replace("\r\n", "\n");
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException("Failed to read file: " + fileName, e);
+        }
     }
 }
