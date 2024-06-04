7682ac93e833ac8b6a2a8ab690ce0d43e8a71cf1
Drago-on <whoareyouwhenyouare@gmail.com>
2024-06-04 19:56:44 +0300

    task 5-0 completed

--- 5-0-functional-programming/5-0-1-lambda-functions-map/src/main/java/com/bobocode/fp/Functions.java
+++ 5-0-functional-programming/5-0-1-lambda-functions-map/src/main/java/com/bobocode/fp/Functions.java
@@ -28,8 +28,14 @@ public class Functions {
     public static FunctionMap<Integer, Integer> intFunctionMap() {
         FunctionMap<Integer, Integer> intFunctionMap = new FunctionMap<>();
 
-        // todo: according to the javadoc add functions using lambda expression
+        intFunctionMap.addFunction("abs", Math::abs);
+        intFunctionMap.addFunction("sgn", Integer::signum);
+        intFunctionMap.addFunction("increment", x -> x + 1);
+        intFunctionMap.addFunction("decrement", x -> x - 1);
+        intFunctionMap.addFunction("square", x -> x * x);
 
         return intFunctionMap;
+        // todo: according to the javadoc add functions using lambda expression
     }
 }
+
